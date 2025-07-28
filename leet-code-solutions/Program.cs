5961bbf7541323d1a01c11dff7f880624fc64a1e
Alonso Quesada <alonq@hotmail.com>
2025-07-28 16:06:57 -0600

    addded longest common prefix problem

--- leet-code-solutions/Program.cs
+++ leet-code-solutions/Program.cs
@@ -1,4 +1,5 @@
-﻿//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+﻿//PROBLEM 1
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //You can return the answer in any order.
 
@@ -43,3 +44,36 @@ int[] TwoSum(int[] nums, int target)
     }
     return Array.Empty<int>();
 }
+
+//PROBLEM 2
+
+//Write a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
+
+//Example 1:
+//Input: strs = ["flower", "flow", "flight"] Output: "fl" Example 2:
+//Input: strs = ["dog", "racecar", "car"] Output: "" Explanation: There is no common prefix among the input strings.
+
+//Constraints:
+//1 <= strs.length <= 200 0 <= strs[i].length <= 200 strs[i] consists of only lowercase English letters if it is non-empty.
+string LongestCommonPrefix(string[] strs)
+{
+    if (strs == null || strs.Length == 0)
+        return "";
+
+    for (int i = 0; i < strs[0].Length; i++)
+    {
+        char c = strs[0][i];
+        for (int j = 1; j < strs.Length; j++)
+        {
+            // If i is out of bounds for strs[j] or characters don't match
+            if (i >= strs[j].Length || strs[j][i] != c)
+                return strs[0].Substring(0, i);
+        }
+    }
+    return strs[0];
+}
+
+var prefix = LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+Console.WriteLine(prefix); // Output: "fl"
+Console.ReadLine();
