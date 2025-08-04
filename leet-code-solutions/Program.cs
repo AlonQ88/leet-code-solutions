ea80fffac30b4b3621f59a6e143dfeacc1b837bf
Alonso Quesada <alonq@hotmail.com>
2025-08-03 19:38:15 -0600

    added IsValidParenthesis problem and solution

--- leet-code-solutions/Program.cs
+++ leet-code-solutions/Program.cs
@@ -151,3 +151,72 @@ foreach (var test in testStrings)
     Console.WriteLine($"Is \"{test}\" a palindrome? {IsPalindrome(test)}");
 }
 Console.ReadLine();
+
+//Valid Parentheses
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+//An input string is valid if:
+
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+//Example 1:
+//Input: s = "()"
+//Output: true
+
+//Example 2:
+//Input: s = "()[]{}"
+//Output: true
+
+//Example 3:
+//Input: s = "(]"
+//Output: false
+
+//Example 4:
+//Input: s = "([])"
+//Output: true
+//Example 5:
+
+//Input: s = "([)]"
+//Output: false
+
+//Constraints:
+
+//1 <= s.length <= 104
+//s consists of parentheses only '()[]{}'.
+
+//[()]
+bool IsValidParenthesis(string s)
+{
+    var stack = new Stack<char>();
+    var map = new Dictionary<char, char>
+    {
+        { ')', '(' },
+        { '}', '{' },
+        { ']', '[' }
+    };
+
+    foreach (char c in s)
+    {
+        if (map.ContainsKey(c))
+        {
+            char topElement = stack.Count == 0 ? '#' : stack.Pop();
+            if (topElement != map[c])
+                return false;
+        }
+        else
+        {
+            stack.Push(c);
+        }
+    }
+    return stack.Count == 0;
+}
+
+// Test IsValidParenthesis
+string[] testParentheses = { "()", "()[]{}", "(]", "([])", "([)]", "{[()]}", "{[()()]}", "((()))", "((())", "(()))" };      
+
+foreach (var test in testParentheses)
+{
+    Console.WriteLine($"Is \"{test}\" valid? {IsValidParenthesis(test)}");
+}
